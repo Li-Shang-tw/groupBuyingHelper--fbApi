491422397699d5fa3259b0f5faafcc4f1fd3592e
root <root@LAPTOP-LQRHI4AS>
2023-11-29 14:36:54 +0800

    refactor:deal hastag to string

--- src/comosables/ShowHashTag.js
+++ src/comosables/ShowHashTag.js
@@ -1,19 +1,18 @@
 export function showHashTag(hastag) {
-  return hastag;
-  // if (typeof hastag == "string") {
-  //   return hastag;
-  // } else {
-  //   let newHashTag = "";
-  //   hastag.forEach((element) => {
-  //     //處理多個商品展現，在中間加上"," 變成字串
-  //     if (newHashTag) {
-  //       newHashTag += " ,";
-  //       newHashTag += element;
-  //     } else {
-  //       //如果newHash是空的，尚未有商品，就不加上，
-  //       newHashTag += element;
-  //     }
-  //   });
-  //   return newHashTag;
-  // }
+  if (typeof hastag == "string") {
+    return hastag;
+  } else {
+    let newHashTag = "";
+    hastag.forEach((element) => {
+      //處理多個商品展現，在中間加上"," 變成字串
+      if (newHashTag) {
+        newHashTag += " ,";
+        newHashTag += element;
+      } else {
+        //如果newHash是空的，尚未有商品，就不加上，
+        newHashTag += element;
+      }
+    });
+    return newHashTag;
+  }
 }
