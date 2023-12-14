699c4b148658b3f315f743d93a7be2935ce57c38
root <root@LAPTOP-LQRHI4AS>
2023-12-14 14:38:57 +0800

    fixed:split comment with + when there is no space between

--- src/comosables/ExtractMessage.js
+++ src/comosables/ExtractMessage.js
@@ -1,7 +1,13 @@
 export function useExtractMessage(message, type) {
   const splitMessage = message.split(" ");
   if (type == "name") {
-    return splitMessage[0];
+    //例外處理 --沒有空格，沒有種類
+
+    if (splitMessage.length === 1) {
+      return message.split("+")[0];
+    } else {
+      return splitMessage[0];
+    }
   } else if (type == "productType") {
     //當字串list長度為3(2以上)，訊息才包含種類
     if (splitMessage.length > 2) {
@@ -10,13 +16,7 @@ export function useExtractMessage(message, type) {
       return "X";
     }
   } else if (type == "number") {
-    //當字串list長度為3
-    if (splitMessage.length < 2) {
-      return splitMessage[2];
-    }
-    //當字串list長度為2
-    else if (splitMessage.length == 2) {
-      return splitMessage[1];
-    }
+    //以+分割，取得數量
+    return message.split("+")[1];
   }
 }
