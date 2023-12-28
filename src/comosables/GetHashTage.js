8185ed7b20696415eb0f2ea1bb31cdfc28e8f1f4
root <root@LAPTOP-LQRHI4AS>
2023-12-28 14:43:40 +0800

    refactor:change let to const

--- src/comosables/GetHashTage.js
+++ src/comosables/GetHashTage.js
@@ -2,14 +2,14 @@ export function useGetHashTage(message) {
   //第一個hastage為商品名
   const hashPosition = message.indexOf("#") + 1;
   //檢查是否有第二個hastage多個商品
-  let leftMessage = message.slice(hashPosition);
+  const leftMessage = message.slice(hashPosition);
   if (leftMessage.includes("#")) {
-    let hashTage = message.substring(hashPosition).split("#");
+    const hashTage = message.substring(hashPosition).split("#");
     //去除空格
     hashTage = hashTage.map((item) => item.trim());
     return hashTage;
   } else {
-    let hashTage = message.substring(hashPosition).trim();
+    const hashTage = message.substring(hashPosition).trim();
 
     return hashTage;
   }
